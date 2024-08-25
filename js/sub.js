cdd02fc7239368b57f09eb2329635ad5c57ad52a
LeeJaMin <sweetmin33@naver.com>
2024-08-26 02:59:31 +0900

    Update sub.js

--- js/sub.js
+++ js/sub.js
@@ -1,41 +1,39 @@
 $(document).ready(function() {
     $(".tab-item").click(function() {
-        // 클릭한 탭을 활성화
         $(".tab-item").removeClass("active");
         $(this).addClass("active");
 
-        // 클릭한 탭에 해당하는 콘텐츠를 표시
         var index = $(this).index();
         $(".tab-content").removeClass("active");
         $(".tab-content:eq(" + index + ")").addClass("active");
     });
 
-
+    $('.tab').click(function () {
+      $('.tab').removeClass('active');
+      $('.tab-pane').removeClass('active');
+      $(this).addClass('active');
+      var tabId = $(this).data('tab');
+      $('#' + tabId).addClass('active');
+  });
+  
     $("#encyclical01 ul li").click(function(){
         $(this).toggleClass("on");
         $('#encyclical01 ul li').not(this).removeClass('on');   
-        $("#encyclical01 ul li .revers").removeClass("on");
-      
+        $("#encyclical01 ul li .revers").removeClass("on");      
      
         let subElement = $(this).find(".content");
 
         if (subElement.is(":visible")) {
-          subElement.slideUp();
-          
+          subElement.slideUp();         
           
         } else {
           $("#encyclical01 ul li .content").slideUp();
-          subElement.slideDown();
-        }
+          subElement.slideDown();        }
 
-        if($(this).hasClass("on")){
-            //$(this).find(".revers > span").html("접어두기");            
+        if($(this).hasClass("on")){        
             $(this).find(".revers").addClass("on");
           } else {
-            $//(this).find(".revers > span").html("펼쳐보기");
             $(this).find(".revers").removeClass("on");
-        }
-        
-       
+        }      
     });
 });
